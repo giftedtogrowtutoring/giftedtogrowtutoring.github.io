6980c697e1a1c8362d9d036737e9088b1f9538b1
giftedtogrowtutoring <giftedtogrowtutoring@gmail.com>
2026-05-29 00:59:12 -0400

    Improve local SEO metadata and structured data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://giftedtogrowtutoring.com",
-  integrations: [sitemap()],
   build: {
     assets: "_assets",
   },
